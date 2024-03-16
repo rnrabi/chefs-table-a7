9dca217ff61edb06fbb716fb9fc0194110b842b3
R N Rabi <rnrabi913@gmail.com>
2024-03-16 18:31:19 +0600

    html designe is ok

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -1,24 +1,26 @@
 import PropTypes from 'prop-types';
+import { GoClock } from "react-icons/go";
+import { AiTwotoneFire } from "react-icons/ai";
 
 
 const Cart = ({crt}) => {
 
-    const {recipe_name , short_description , ingredients} = crt;
+    const {recipe_name , short_description , ingredients, image} = crt;
 
 
     return (
         <div className="">
 
-            <div className="card w-full bg-base-100 shadow-xl">
+            <div className="card w-full h-full bg-base-100 shadow-xl">
 
-             <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+             <figure><img className='w-full h-56' src={image} alt="Shoes" /></figure>
 
             <div className="space-y-5">
                 <h2 className="card-title">{recipe_name}</h2>
                 <p>{short_description}</p>
                 <div>
                     <h2>Ingreadients:{ingredients.length}</h2>
-                    <ul className='list-disc'>
+                    <ul className='list-disc pl-4'>
                        {
                         ingredients.map((ingre , index) =>(
                             <li key={index}>{ingre}</li>
@@ -28,12 +30,12 @@ const Cart = ({crt}) => {
                 </div>
 
                 <div className="flex justify-between">
-                    <p>30 min</p>
-                    <p>600 calories</p>
+                    <p className="flex items-center gap-2"><GoClock></GoClock>30 min</p>
+                    <p className="flex items-center gap-2"> <AiTwotoneFire></AiTwotoneFire> 600 calories</p>
                 </div>
 
                 <div className="card-actions">
-                <button className="btn btn-primary rounded-full">want ot cook</button>
+                <button className="btn btn-primary rounded-full">want to cook</button>
                 </div>
             </div>
 
--- src/Component/Recipes/Recipes.jsx
+++ src/Component/Recipes/Recipes.jsx
@@ -17,7 +17,7 @@ const Recipes = () => {
             <h2 className="text-center text-3xl text-black">Our Recipes</h2>
             <p className="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque adipisci sed totam omnis animi at obcaecati eum nobis, eaque eveniet.</p>
 
-            <div className="grid grid-cols-12">
+            <div className="grid grid-cols-12 gap-5">
                  <div className="col-span-8 grid grid-cols-2 justify-between gap-10">
                     {
                         chefCart.map(crt =>(
@@ -30,7 +30,32 @@ const Recipes = () => {
                 </div>
 
                 <div className="col-span-4">
-                <p>This is test cols 4</p>
+                <h2 className="text-2xl text-center">Want to clock: 0</h2>
+                <div className="overflow-x-auto">
+                        <table className="table">
+                            {/* head */}
+                            <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Time</th>
+                                <th>Calories</th>
+                            </tr>
+                            </thead>
+                            
+                            <tbody>
+                            {/* row 1 */}
+
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
 
